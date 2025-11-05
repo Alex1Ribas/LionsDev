57c9be040b4faf5dd0b9d641245033122853f6a1
alex ribas <alex.ribas12@gmail.com>
2025-11-05 11:14:18 -0300

    fix: update id

--- MongoDB/Alugueis_MongoDB/livros/updateLivros.js
+++ MongoDB/Alugueis_MongoDB/livros/updateLivros.js
@@ -1,9 +1,6 @@
-const updateLivros = async (title, author, year, genre) => {
+const updateLivros = async (id, title, author, year, genre) => {
     try{
-        const updateLivros = await book.findByIdAndUpdate(
-            id,
-            {title, author, year, genre},
-            {new: true, runValidators: true}
+        const updateLivros = await book.findByIdAndUpdate(id,{title, author, year, genre},{new: true, runValidators: true}
         );
         return updateLivros;
     } catch (error){
