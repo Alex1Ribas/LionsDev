3f72a63cfeba54aa1dba14534bdc454ea704581c
Alex <alex.ribas12@gmail.com>
2025-12-07 15:03:59 -0300

    feat: add receita controller with CRUD operations

---
+++ ProjetoFinal/src/controllers/receita.controller.js
@@ -0,0 +1,49 @@
+import receitaService from "../services/receita.service.js";
+
+export const receitaController = {
+
+  async create(req, res, next) {
+    try {
+      const result = await receitaService.criarReceita(req.body);
+      return res.status(201).json(result);
+    } catch (err) {
+      return next(err);
+    }
+  },
+
+  async list(req, res, next) {
+    try {
+      const result = await receitaService.listarReceitas();
+      return res.json(result);
+    } catch (err) {
+      return next(err);
+    }
+  },
+
+  async search(req, res, next) {
+    try {
+      const result = await receitaService.buscarReceitaPorPagante(req.params);
+      return res.json(result);
+    } catch (err) {
+      return next(err);
+    }
+  },
+
+  async update(req, res, next) {
+    try {
+      const result = await receitaService.atualizarReceita(req.params, req.body);
+      return res.json(result);
+    } catch (err) {
+      return next(err);
+    }
+  },
+
+  async remove(req, res, next) {
+    try {
+      const result = await receitaService.deletarReceita(req.params);
+      return res.json(result);
+    } catch (err) {
+      return next(err);
+    }
+  },
+};
