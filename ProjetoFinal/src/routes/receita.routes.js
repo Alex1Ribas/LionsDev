b1644de8f91b882851dbe6cbffe145ce893a0f9f
Alex <alex.ribas12@gmail.com>
2025-12-07 15:02:44 -0300

    feat: add receita routes with authentication and authorization

---
+++ ProjetoFinal/src/routes/receita.routes.js
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import { ensureValidId } from "../middlewares/validate.middleware.js";
+import { authMiddleware, requireRole } from '../middlewares/authMiddleware.js';
+import { receitaController } from '../controllers/receita.controller.js';
+
+const router = Router();
+
+router.post('/receita', authMiddleware(), requireRole("ADMIN"), receitaController.create);
+router.get('/receita', authMiddleware(), requireRole("ADMIN"),receitaController.list);
+router.get('/receita/:id', authMiddleware(), ensureValidId, receitaController.search);
+router.put('/receita/:id', authMiddleware(), ensureValidId, ensureValidId, receitaController.update);
+router.delete('/receita/:id', authMiddleware(), ensureValidId,requireRole("ADMIN"), receitaController.remove);
+
+export default router;
