5ec0ba809d313781c171843805ca2719afefa1f6
Alex <alex.ribas12@gmail.com>
2025-12-07 15:03:25 -0300

    feat: add despesa routes with authentication and role-based access control

---
+++ ProjetoFinal/src/routes/despesa.routes.js
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import { ensureValidId } from "../middlewares/validate.middleware.js";
+import { authMiddleware, requireRole } from '../middlewares/authMiddleware.js';
+import { despesaController } from '../controllers/despesa.controller.js'
+
+const router = Router();
+
+router.post('/despesa', authMiddleware(), requireRole("ADMIN"), despesaController.create);
+router.get('/despesa', authMiddleware(), requireRole("ADMIN"),despesaController.list);
+router.get('/despesa:id', authMiddleware(), ensureValidId, despesaController.search);
+router.put('/despesa/:id', authMiddleware(), ensureValidId, ensureValidId, despesaController.update);
+router.delete('/despesa/:id', authMiddleware(), ensureValidId,requireRole("ADMIN"), despesaController.remove);
+
+export default router;
