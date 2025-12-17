609e435f6b8eb28e8edbcc7f434a25b8bc76d865
Alex <alex.ribas12@gmail.com>
2025-12-17 19:59:46 -0300

    refactor: simplificate route

--- ProjetoFinal/src/controllers/relatorio.controller.js
+++ ProjetoFinal/src/controllers/relatorio.controller.js
@@ -2,62 +2,14 @@ import relatorioService from "../services/relatorio.service.js";
 
 export const relatorioController = {
   async create(req, res, next) {
-    try {
-      const user = req.user;
-      const result = await relatorioService.criarRelatorio({
-        ...req.body,
-        user,
+   try {
+      const { tipo, periodoInicio, periodoFim } = req.body;
+      const user = req.user
+
+      const result = await relatorioService.create({
+        user, tipo, periodoInicio, periodoFim
       });
       return res.status(201).json(result);
-    } catch (err) {
-      return next(err);
-    }
-  },
-
-  async list(req, res, next) {
-    try {
-      const user = req.user;
-      const result = await relatorioService.listarRelatorio({ user });
-      return res.json(result);
-    } catch (err) {
-      return next(err);
-    }
-  },
-
-  async search(req, res, next) {
-    try {
-      const result = await relatorioService.buscarRelatorio({
-        ...req.params,
-        user,
-      });
-      return res.json(result);
-    } catch (err) {
-      return next(err);
-    }
-  },
-
-  async update(req, res, next) {
-    try {
-      const user = req.user;
-      const result = await relatorioService.atualizarRelatorio({
-        ...req.params,
-        ...req.body,
-        user,
-      });
-      return res.json(result);
-    } catch (err) {
-      return next(err);
-    }
-  },
-
-  async remove(req, res, next) {
-    try {
-      const user = req.user;
-      const result = await relatorioService.deletarRelatorio({
-        ...req.params,
-        user,
-      });
-      return res.json(result);
     } catch (err) {
       return next(err);
     }
--- ProjetoFinal/src/routes/relatorio.routes.js
+++ ProjetoFinal/src/routes/relatorio.routes.js
@@ -1,13 +1,8 @@
 import { Router } from 'express';
-import { ensureValidId } from "../middlewares/validate.middleware.js";
-import { authMiddleware, requireRole } from '../middlewares/authMiddleware.js';
+import { authMiddleware } from '../middlewares/authMiddleware.js';
 import { relatorioController } from '../controllers/relatorio.controller.js';
 const router = Router();
 
 router.post('/relatorio/', authMiddleware, relatorioController.create);
-router.get('/relatorio/', authMiddleware,relatorioController.list);
-router.get('/relatorio/:id', authMiddleware, ensureValidId, relatorioController.search);
-router.put('/relatorio/:id', authMiddleware, ensureValidId, ensureValidId, relatorioController.update);
-router.delete('/relatorio/:id', authMiddleware, ensureValidId,requireRole("ADMIN"), relatorioController.remove);
 
 export default router;
