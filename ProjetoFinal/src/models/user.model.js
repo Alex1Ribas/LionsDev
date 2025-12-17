06186fbf6a20369ea5b4fe05d13c8ac972c544f7
Alex <alex.ribas12@gmail.com>
2025-12-17 09:44:38 -0300

    remove: city and sector for now. update: role default "ADMIN"

--- ProjetoFinal/src/models/user.model.js
+++ ProjetoFinal/src/models/user.model.js
@@ -2,13 +2,10 @@ import mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema({
     name:    {type: String, require:true, trim: true},
-    city:    {type: String, require:true, trim: true},  
-    sector:  {type: String, require:true, trim: true},
     email:   {type: String, require:true, trim: true, unique: true, lowerase: true},
     password:{type: String, require:true, select: false },
     confPassword:{type: String, require:true, select: false },
-
-    Role:    {type: String, enum: ["ADMIN", "USER"], default: "USER"}
+    Role:    {type: String, enum: ["ADMIN", "USER"], default: "ADMIN"}
 }, {timestamps: true});
 
 export default mongoose.model('User', UserSchema);
