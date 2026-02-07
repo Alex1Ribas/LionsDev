898e44142b875e13f9c2cad73a8827f2aa80c79e
Alex <alex.ribas12@gmail.com>
2026-02-07 19:13:14 -0300

    remove: delete multiple unused JavaScript files from modulo_1

--- modulo_1/Calculadora/divisao.js
+++
@@ -1,8 +0,0 @@
-function divisao (num1, num2){
-    if(num2 === 0){
-        console.log('Impossivel dividir por zero')
-    }
-    return num1/num2
-}
-
-module.exports = divisao
--- modulo_1/Calculadora/entrada.js
+++
@@ -1,7 +0,0 @@
-const prompt = require('prompt-sync')()
-
-function lerEntrada(mensagem){
-    return prompt(mensagem)
-}
-
-module.exports = lerEntrada
