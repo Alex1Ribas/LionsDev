2ac8a0d3cfdcbc27665781c0393e48c916284e32
Alex <alex.ribas12@gmail.com>
2026-02-13 11:59:25 -0300

    feat: add module-01

---
+++ module-01/processTwoNumbers.js
@@ -0,0 +1,10 @@
+let value1 = 10;
+let value2 = 20;
+
+let sum = value1 + value2;
+let average = sum / 2;
+console.log(`Calculated average: ` + average);
+
+let extraScore = average * 3;
+let passedTheCut = extraScore === 10;
+console.log(passedTheCut);
